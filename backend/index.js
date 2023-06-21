b0fa6ad8630e2c06491d98b2065ec0f19c2d497d
Arjun Patel <hackmech007@gmail.com>
2023-06-21 19:47:00 +0530

    post request

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,12 @@ connectToMongo();
 
 const app = express();
 const port = 3000;
+app.use(express.json());
 
-app.get("/", (req,res)=> {
-    res.send("Hello World 1234");
-})
+//Available Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, ()=> {
-    console.log("App listening at port " + port);
+    console.log("App listening at port http://localhost:" + port);
 })
