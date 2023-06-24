ea2d9ade60d011c976c4534e03d985ceb88c9d53
Arjun Patel <hackmech007@gmail.com>
2023-06-24 17:09:54 +0530

    useContext and useLocation added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import About from "./components/About";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
+      <NoteState>
+        <BrowserRouter>
+          <Navbar />
+          <div className="container">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/about" element={<About />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </NoteState>
     </>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useContext, useEffect} from 'react';
+import noteContext from '../context/notes/noteContext';
 
 function About() {
+    
   return (
     <div>
-      This is about.
+      This is about page
     </div>
   )
 }
