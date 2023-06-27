4a33830fa42e30e879c66f7f8ed8486ff53b638a
Arjun Patel <hackmech007@gmail.com>
2023-06-27 16:44:27 +0530

    fetching notes from object using context

--- backend/index.js
+++ backend/index.js
@@ -11,5 +11,5 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, ()=> {
-    console.log("App listening at port http://localhost:" + port);
+    console.log("iNotebook App listening at port http://localhost:" + port);
 })
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-    
+    const notesInitial = [
+        {
+            "_id": "649592205b0ee53668c40112",
+            "user": "64957a0d378e9303cccd6e8a",
+            "title": "my-title",
+            "description": "wake up at 6'o clock",
+            "tag": "personal",
+            "date": "2023-06-23T12:37:52.379Z",
+            "__v": 0
+        },
+        {
+            "_id": "6495986377a7d0ae50f6d4ce",
+            "user": "64957a0d378e9303cccd6e8a",
+            "title": "new post",
+            "description": "delete route adding in ednpoints",
+            "tag": "facebook",
+            "date": "2023-06-23T13:04:35.331Z",
+            "__v": 0
+        },
+        {
+            "_id": "64959976968272c8ce574d11",
+            "user": "64957a0d378e9303cccd6e8a",
+            "title": "new post",
+            "description": "delete route adding in ednpoints",
+            "tag": "facebook",
+            "date": "2023-06-23T13:09:10.918Z",
+            "__v": 0
+        }
+    ]
+
+    const [notes, setNotes] = useState(notesInitial)
+
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     );
