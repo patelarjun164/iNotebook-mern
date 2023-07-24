c88f4605ebb4c127c6842aeb359e1ba2f0062d91
Arjun Patel <92094326+patelarjun164@users.noreply.github.com>
2023-07-24 19:44:33 +0530

    Update db.js mongouri

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://127.0.0.1:27017/iNotebook";
+const mongoURI = "mongodb+srv://hackmech007:1111arjun@cluster0.tzmxqm3.mongodb.net/iNotebook";
 
 const connectToMongo = async () => {
     try {
